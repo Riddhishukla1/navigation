e4c7548b9ec2478fad586959f6042fdbe4a8a35f
Riddhishukla1 <128504209+Riddhishukla1@users.noreply.github.com>
2023-03-22 13:34:51 -0400

    add form

---
+++ navigation/navigation/formViewController.swift
@@ -0,0 +1,32 @@
+//
+//  formViewController.swift
+//  navigation
+//
+//  Created by Canadore Student on 2023-03-22.
+//
+
+import UIKit
+
+class formViewController: UIViewController {
+
+    @IBOutlet weak var firstname: UITextField!
+    @IBOutlet weak var lastname: UITextField!
+    @IBOutlet weak var email: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ navigation/navigation/infoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  infoViewController.swift
+//  navigation
+//
+//  Created by Canadore Student on 2023-03-22.
+//
+
+import UIKit
+
+class infoViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    var firstname = " ", lastname=" ", email=" "
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label1.text = "firt name:" + firstname
+        label2.text = "last name:" + lastname
+        label3.text = "email:" + email
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
