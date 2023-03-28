f283474743c137001034b99380f9baddf63bc798
Riddhishukla1 <128504209+Riddhishukla1@users.noreply.github.com>
2023-03-28 08:41:56 -0400

    changes in design

--- navigation/navigation/infoViewController.swift
+++ navigation/navigation/infoViewController.swift
@@ -12,6 +12,10 @@ class infoViewController: UIViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    @IBOutlet weak var label6: UILabel!
+    @IBOutlet weak var label7: UILabel!
     var firstname = " ", lastname=" ", email=" "
     override func viewDidLoad() {
         super.viewDidLoad()
